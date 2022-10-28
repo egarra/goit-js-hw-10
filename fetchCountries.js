a633b27b52fc0045c9909db84e092c8caf97ab93
egarra <garikhabuzov@gmail.com>
2022-10-28 13:59:00 +0300

    search create. need fixes

---
+++ fetchCountries.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/'
+const fields = 'fields=name,capital,population,flags,languages'
+
+const fetchCountries = (name) => {
+    return fetch(`${BASE_URL}${name}?${fields}`)
+        .then(response => response.json())
+        .catch(error => console.log(error))
+    }
+
+export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import fetchCountries from '../fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const inputCountry = document.querySelector('#search-box'),
+      countryList = document.querySelector('.country-list'),
+      countryInfo = document.querySelector('.country-info');
+
+inputCountry.addEventListener('input', 
+    debounce( () => {
+        const name = inputCountry.value.trim();
+
+        if (name === '') {
+             countryList.innerHTML = '' 
+             countryInfo.innerHTML = ''
+          }
+
+        fetchCountries(name)
+            .then((countries) => {
+                countryList.innerHTML = '' 
+                countryInfo.innerHTML = ''
+
+                if (countries.length === 1) {
+                    onCountryList(countries)
+                    onCountryInfo(countries)
+                } else if (countries.length > 10) {
+                    Notify.info('Too many matches found. Please enter a more specific name.')
+                } else {
+                    onCountryList(countries)
+                }
+                
+            })
+            .catch((error) => console.log(error))
+        },
+    DEBOUNCE_DELAY)
+        
+)
+
+function onCountryList (countries) {
+    countries.forEach(({name, flags}) => {
+        countryList.insertAdjacentHTML('afterbegin', 
+                `
+                <li class="country-list__item">
+                <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+                <h2 class="country-list__name">${name.official}</h2>
+                </li>
+        `)
+    })
+}
+
+function onCountryInfo (countries) {
+    countries.forEach(({ capital, population, languages }) => {
+        countryList.insertAdjacentHTML('afterend', 
+                `
+                <ul class="country-info__list">
+                <li class="country-info__item"><p><b>Capital: </b>${capital}</p></li>
+                <li class="country-info__item"><p><b>Population: </b>${population}</p></li>
+                <li class="country-info__item"><p><b>Languages: </b>${Object.values(languages).join(', ')}</p></li>
+        </ul>
+        `)
+    })
+}
+
